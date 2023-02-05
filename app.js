3d1172476842c7d72d5556e3a5ce4018fd2340a1
SandraVLP <aleksanvp@yandex.ru>
2023-02-05 20:22:06 +0300

    link

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.post('/signup', celebrate({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri(),
+    avatar: Joi.string().link(),
     about: Joi.string().min(2).max(30),
   }),
 }), createUser);
