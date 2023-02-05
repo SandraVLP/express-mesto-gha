96980a2743f72d4eceb309d9c953d6a4aa0660bd
SandraVLP <aleksanvp@yandex.ru>
2023-02-05 19:48:37 +0300

    signup/in

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const express = require('express');
 const { PORT = 3000 } = process.env;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { celebrate, Joi } = require('celebrate');
 
 const app = express();
 const { errors } = require('celebrate');
+const {
+  login, createUser,
+} = require('./controllers/user');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,6 +18,24 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use('/users', require('./routes/user'));
 app.use('/cards', require('./routes/card'));
+
+app.post('/signin', celebrate({
+  // params: Joi.string().pattern(),
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  // params: Joi.string().pattern(),
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    avatar: Joi.string().uri(),
+    about: Joi.string().min(2).max(30),
+  }),
+}), createUser);
 
 app.use(errors()); // обработчик ошибок celebrate
 app.use((err, req, res, next) => {
